d077666f4d40e01e706fc1732c3ac26ae1448871
FlameCodex311 <flamecodex311@gmail.com>
2021-09-09 19:52:23 -0400

    Color refactoring

--- Competition Automation Scripts/Linux (bash)/FlameScript.sh
+++ Competition Automation Scripts/Linux (bash)/FlameScript.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 clear
-echo Installing applications...
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+BLUE="\e[1;34m"
+PURPLE="\e[1;35m"
+CYAN="\e[1;36m"
+WHITE="\e[1;37m"
+ENDLN="\e[0m"
+
+echo -e "$WHITE Installing applications... $ENDLN"
 clear
 sudo apt-get update &> /dev/null && sudo apt-get install gufw auditd clamav libpam-cracklib gnome-tweaks &> /dev/null
 rm /home/$USER/groupinfo.txt &> /dev/null
@@ -11,198 +20,205 @@ software-properties-gtk &> /dev/null & return &> /dev/null
 
 view_passwd() {
   clear
-  echo -------------------------------passwd-------------------------------
+  echo -e "$WHITE -------------------------------passwd------------------------------- $ENDLN"
   cat /etc/passwd
-  echo --------------------------------------------------------------------
+  echo -e "$WHITE -------------------------------------------------------------------- $ENDLN"
   echo ""
-  echo Press any key to return...
+  echo -e "$GREEN Completed Successfully! $ENDLN"
+  echo ""
+  echo -e "$WHITE Press any key to return... $ENDLN"
 }
 
 view_group() {
   clear
-  echo --------------group--------------
+  echo -e "$WHITE --------------group-------------- $ENDLN"
   cat /etc/group
-  echo ----------------------------------
+  echo -e "$WHITE ---------------------------------- $ENDLN"
   echo ""
-  echo Press any key to return...
+  echo -e "$GREEN Completed Successfully! $ENDLN"
+  echo ""
+  echo -e "$WHITE Press any key to return... $ENDLN"
 }
 
 view_usr() {
   clear
-  echo "Which user would you like to inspect?"
+  echo -e "$WHITE Which user would you like to inspect? $ENDLN"
   echo ""
   read uservar
   groups $uservar > /dev/null
   if [ "$?" = "1" ]; then
     clear
-    echo ---------------
-    echo Invalid User!!!
-    echo ---------------
+    echo -e "$RED --------------- $ENDLN"
+    echo -e "$RED Invalid User!!! $ENDLN"
+    echo -e "$RED --------------- $ENDLN"
     echo ""
-    echo Press any key to return...
+    echo -e "$WHITE Press any key to return... $ENDLN"
   else
     clear
-    echo the user $uservar is in the groups:
-    echo -----------------------------------
+    echo -e "$WHITE the user $uservar is in the groups: $ENDLN"
+    echo -e "$WHITE ----------------------------------- $ENDLN"
     groups $uservar
-    echo -----------------------------------
+    echo -e "$WHITE ----------------------------------- $ENDLN"
     echo ""
-    echo Press any key to return...
+    echo -e "$WHITE Press any key to return... $ENDLN"
   fi
 }
 
 view_strings() {
   clear
-  echo "Which database would you like to explore? (passwd, group, shadow)"
+  echo -e "$WHITE Which database would you like to explore? (passwd, group, shadow) $ENDLN"
   echo ""
   read database
   case "$database" in
     passwd | group | shadow)
       clear
       echo ""
-      echo "Which key would you like to use for the database" $database "(use none if you dont know)"?
+      echo -e "$WHITE Which key would you like to use for the database" $database "(use none if you dont know) $ENDLN"?
       echo ""
       read key
       clear
       getent $database $key
+      echo -e "$GREEN Completed Successfully! $ENDLN"
       echo ""
-      echo Press any key to return...
+      echo -e "$WHITE Press any key to return... $ENDLN"
       ;;
     *)
       clear
-      echo ---------------
-      echo Invalid Database!!!
-      echo ---------------
+      echo -e "$RED --------------- $ENDLN"
+      echo -e "$RED Invalid Database!!! $ENDLN"
+      echo -e "$RED --------------- $ENDLN"
       echo ""
-      echo Press any key to return...
+      echo -e "$WHITEPress any key to return... $ENDLN"
       ;;
   esac
 }
 
 file_properties() {
   clear
-  echo "Enter the path to the file you want to explore:"
+  echo -e "$WHITE Enter the path to the file you want to explore: $ENDLN"
   echo ""
   read filepath
   ls -l $filepath &> /dev/null
   if [ $? = 2 ]; then
     clear
-    echo ---------------
-    echo Invalid filepath!!!
-    echo ---------------
-    echo Press any key to return...
+    echo -e "$RED --------------- $ENDLN"
+    echo -e "$RED Invalid filepath!!! $ENDLN"
+    echo -e "$RED --------------- $ENDLN"
+    echo -e "$WHITE Press any key to return... $ENDLN"
   else
     echo ""
     ls -l $filepath
+    echo -e "$GREEN Completed Successfully! $ENDLN"
     echo ""
-    echo Press any key to return...
+    echo -e "$WHITE Press any key to return... $ENDLN"
   fi
 }
 
 addusers() {
   clear
-  echo "Would you like to add or remove a user? (add/remove)"
+  echo -e "$WHITE Would you like to add or remove a user? (add/remove) $ENDLN"
   echo ""
   read options1
   echo ""
   if [ "$options1" = "add" ]; then
-    echo Enter the name of the user you want to add
+    echo -e "$WHITE Enter the name of the user you want to add $ENDLN"
     echo ""
     read userEntered
     echo ""
     adduser $userEntered
-    echo Completed Successfully!
+    echo -e "$GREEN Completed Successfully! $ENDLN"
+    echo ""
+    echo -e "$WHITE Press any key to return... $ENDLN"
   elif [ "$options1" = "remove" ]; then
-    echo Enter the name of the user you want to remove
+    echo -e "$WHITE Enter the name of the user you want to remove $ENDLN"
     echo ""
     read userRemove
     echo ""
     deluser $userRemove
-    echo Completed Successfully!
+    echo -e "$GREEN Completed Successfully! $ENDLN"
     echo ""
-    echo Press any key to return...
+    echo -e "$WHITE Press any key to return... $ENDLN"
   else
-    echo Invalid input
+    echo -e "$RED Invalid input $ENDLN"
     echo ""
-    echo Press any key to return...
+    echo -e "$WHITE Press any key to return... $ENDLN"
   fi
 }
 
 addrmgroup() {
   clear
-  echo "Do you want to add or delete a group? (add/remove)"
+  echo -e "$WHITE Do you want to add or delete a group? (add/remove) $ENDLN"
   echo ""
   read options3
   echo ""
   if [ "$options3" = "add" ]; then
-    echo "Enter the name of the group you want to create"
+    echo -e "$WHITE Enter the name of the group you want to create $ENDLN"
     echo ""
     read options4
     echo ""
     groupadd $options4
-    echo "Completed Successfully!"
+    echo -e "$GREEN Completed Successfully! $ENDLN"
   elif [ "$options3" = "remove" ]; then
-    echo "Enter The name of the group you want to remove"
+    echo -e "$WHITE Enter The name of the group you want to remove $ENDLN"
     echo ""
     read options5
     echo ""
     groupdel $options5
-    echo "Completed Successfully!"
+    echo -e "$GREEN Completed Successfully! $ENDLN"
     else
-    echo "Invalid input"
+    echo -e "$RED Invalid input $ENDLN"
   fi
 }
 
 addrmusergroup() {
   clear
-  echo "Do you want to add or delete a user from a group? (add/remove)"
+  echo -e "$WHITE Do you want to add or delete a user from a group? (add/remove) $ENDLN"
   echo ""
   read options6
   echo ""
   if [ "$options6" = "add" ]; then
-    echo "Enter the name of the user you want to add to a group"
+    echo "$WHITE Enter the name of the user you want to add to a group $ENDLN"
     echo ""
     read options7
     echo ""
-    echo "Enter the group you want to add them to"
+    echo -e "$WHITE Enter the group you want to add them to $ENDLN"
     echo ""
     read options8
     echo ""
     gpasswd -a $options7 $options8
-    echo "Completed Successfully!"
+    echo -e "$GREEN Completed Successfully! $ENDLN"
   elif [ "$options6" = "remove" ]; then
-    echo "Enter the name of the user you want to remove from a group"
+    echo -e "$WHITE Enter the name of the user you want to remove from a group $ENDLN"
     echo ""
     read options9
     echo ""
-    echo "Enter the name of the group you want to remove them from"
+    echo -e "$WHITE Enter the name of the group you want to remove them from $ENDLN"
     echo ""
     read options10
     echo ""
     gpasswd -d $options9 $options10
-    echo "Completed Successfully!"
+    echo -e "$GREEN Completed Successfully! $ENDLN"
     else
-    echo "Invalid input"
+    echo -e "$RED Invalid input $ENDLN"
   fi
 }
 
 changepasswd () {
   clear
-  echo "Enter the name of the user whos password you want to change"
+  echo -e "$WHITE Enter the name of the user whos password you want to change $ENDLN"
   echo ""
   read userpasswd
-  echo "",lydia
-
+  echo ""
   passwd $userpasswd
   echo ""
-  echo "Completed Successfully!"
+  echo -e "$GREEN Completed Successfully! $ENDLN"
 }
 
 upgrade () {
   clear
   apt-get update && apt-get upgrade
   echo ""
-  echo "Completed Successfully!"
+  echo -e "$GREEN Completed Successfully! $ENDLN"
 }
 
 security() {
@@ -218,7 +234,7 @@ security() {
   sudo cp -f common-auth /etc/pam.d/common-auth
   sudo cp -f common-account /etc/pam.d/common-account
   sudo cp -f users.conf /etc/lightdm/users.conf
-  echo "Completed Successfully!"
+  echo -e "$GREEN Completed Successfully! $ENDLN"
 }
 
 tweaksrun() {
@@ -228,7 +244,7 @@ tweaksrun() {
 
 findmedia() {
   clear
-  echo Finding media files...
+  echo "$WHITE Finding media files... $ENDLN"
   sudo find /home -type f -iname "*.mp3" > /home/$USER/mediafiles.txt
   sudo find /home -type f -iname "*.avi" >> /home/$USER/mediafiles.txt
   sudo find /home -type f -iname "*.mov" >> /home/$USER/mediafiles.txt
@@ -240,7 +256,7 @@ findmedia() {
   sudo find /home -type f -iname "*.flac" >> /home/$USER/mediafiles.txt
   sudo find /home -type f -iname "*.flac" >> /home/$USER/mediafiles.txt
   echo ""
-  echo "Completed Successfully!"
+  echo -e "$GREEN Completed Successfully! $ENDLN"
 }
 
 runneo() {
@@ -251,7 +267,7 @@ runneo() {
 }
 
 menu(){
-echo -e "\e[0;34m
+echo -e "$CYAN
 ███████╗██╗      █████╗ ███╗   ███╗███████╗███████╗ ██████╗██████╗ ██╗██████╗ ████████╗
 ██╔════╝██║     ██╔══██╗████╗ ████║██╔════╝██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝
 █████╗  ██║     ███████║██╔████╔██║█████╗  ███████╗██║     ██████╔╝██║██████╔╝   ██║
@@ -259,14 +275,14 @@ echo -e "\e[0;34m
 ██║     ███████╗██║  ██║██║ ╚═╝ ██║███████╗███████║╚██████╗██║  ██║██║██║        ██║
 ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝
 
-///////////////////////////////////  Version 0.1.5  ///////////////////////////////////\e[0m"
+///////////////////////////////////  Version 0.1.5  ///////////////////////////////////$ENDLN"
 echo ""
-echo -e "\e[0;31m*****************************************************************\e[0m"
-echo -e "\e[0;31m*** DO THE FORENSICS QUESTIONS BEFORE MESSING WITH THE SYSTEM ***\e[0m"
-echo -e "\e[0;31m*****************************************************************\e[0m"
+echo -e "$RED*****************************************************************$ENDLN"
+echo -e "$RED*** DO THE FORENSICS QUESTIONS BEFORE MESSING WITH THE SYSTEM ***$ENDLN"
+echo -e "$RED*****************************************************************$ENDLN"
 echo ""
-echo -e "\e[0;33mThe current working directory is: $PWD\e[0m"
-echo -e "\e[0;37m
+echo -e "$YELLOW The current working directory is: $PWD $ENDLN"
+echo -e "$WHITE
 1) View passwd file
 2) View group file
 3) View a users groups
@@ -278,14 +294,14 @@ echo -e "\e[0;37m
 9) Change a users password
 10) Update repositories and upgrade packages, also scan for viruses
 11) Update security settings
-12) Run gnome-tweaks
+12) Run gnome-tweaks (used for startup applications)
 13) Display system information
 14) Scan for media files
-0) Exit
+0) Exit $ENDLN
 
-\e[0;34m////////////////////////////////////////////////////////////////////////////////////////\e[0m
+$CYAN////////////////////////////////////////////////////////////////////////////////////////$ENDLN
 
-\e[0;36mChoose an option: \e[0m"
+$WHITE Choose an option: $ENDLN"
         read a
         echo ""
         case $a in
